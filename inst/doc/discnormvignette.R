3b9a086c4e0cb00e025d35ead00b3ce06fc29385
Njål Foldnes <njal.foldnes@gmail.com>
2021-12-10 07:20:05 +0000

    version 0.1.1

--- inst/doc/discnormvignette.R
+++ inst/doc/discnormvignette.R
@@ -1,13 +1,13 @@
-## ---- include = FALSE----------------------------------------------------
+## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----setup---------------------------------------------------------------
+## ----setup--------------------------------------------------------------------
 library(discnorm )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #let us discretize an underlying normal vector
 # with moderate correlation 
 rho <- 0.3
@@ -23,7 +23,7 @@ pvalue <- bootTest(disc.sample, B=500)
 print(pvalue)
 # we have no evidence against the null hypothesis of underlying normality
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 nonnorm.sample <- data.frame(norm.sample[, 1:4], norm.sample[,1]*norm.sample[,2])
 disc.sample2 <- apply(nonnorm.sample, 2, cut, breaks=c(-Inf, -1, 1, 2, Inf), labels=FALSE)
 pvalue <- bootTest(disc.sample2, B=500)
